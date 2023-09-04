76d4e54c0d8cf7ef9debc6f60d08dbcd180b137e
Github <emyy5>
2023-09-04 06:03:07 +0300

    First

--- ScrollViewDemo/ViewController.swift
+++ ScrollViewDemo/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width + 1000, height: UIScreen.main.bounds.height + 1000)
     }
 
 
